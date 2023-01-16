c26e374f1e3b32d5fa96848c6f98d3ab96dc23f9
Wes Keiser <wes@weskeiser.no>
2023-01-16 08:45:35 +0100

    initial commit

---
+++ src/main/java/com/moviecorp/noroffproject/NoroffprojectApplication.java
@@ -0,0 +1,13 @@
+package com.moviecorp.noroffproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoroffprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoroffprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviecorp/noroffproject/NoroffprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviecorp.noroffproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoroffprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
