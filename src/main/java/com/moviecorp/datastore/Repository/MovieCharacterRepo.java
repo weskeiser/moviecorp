2206fdda1a7cfd618c2f959eab68ce7a1aa349ac
Wes Keiser <wes@weskeiser.no>
2023-02-02 14:54:31 +0100

    fix: change repository type

--- src/main/java/com/moviecorp/datastore/Repository/MovieCharacterRepo.java
+++ src/main/java/com/moviecorp/datastore/Repository/MovieCharacterRepo.java
@@ -1,9 +1,8 @@
 package com.moviecorp.datastore.Repository;
 
 import com.moviecorp.datastore.Entity.MovieCharacter;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieCharacterRepo extends CrudRepository<MovieCharacter, Long> {}
+public interface MovieCharacterRepo extends JpaRepository<MovieCharacter, Long> {}
