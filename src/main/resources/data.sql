fc9fee980aaf14c2d6aea2f988f2d85399da79f5
Wes Keiser <wes@weskeiser.no>
2023-02-02 14:59:29 +0100

    fix: add proper init data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,10 @@
-INSERT INTO movie (title, genre, director, release_year) VALUES ('some name', 'some genre', 'some director', 1994);
+INSERT INTO movie (title, genre, director, release_year, picture_url, trailer_url) VALUES ('A New Hope', 'Science Fiction', 'George Lucas', 1994, 'https://lumiere-a.akamaihd.net/v1/images/EP4_POS_2_D-RESIZED_f977074a.jpeg', 'https://www.imdb.com/video/vi1317709849/?playlistId=tt0076759&ref_=tt_ov_vi');
+INSERT INTO movie (title, genre, director, release_year, picture_url, trailer_url) VALUES ('The Godfather', 'Drama', 'Francis Ford Coppola', 1972, 'https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i', 'https://www.imdb.com/video/vi1348706585/?playlistId=tt0068646&ref_=tt_pr_ov_vi');
+INSERT INTO movie (title, genre, director, release_year, picture_url, trailer_url) VALUES ('The Rise of Skywalker', 'Science Fiction', 'J. J. Abrams', 2019, 'https://lumiere-a.akamaihd.net/v1/images/p_starwarstheriseofskywalker_18508_3840c966.jpeg', 'https://www.imdb.com/video/vi518503961/?playlistId=tt2527338&ref_=tt_pr_ov_vi');
+
+INSERT INTO character (full_name, gender, picture_url) VALUES ('Babu Frik', 'male', 'https://static.wikia.nocookie.net/starwars/images/b/b8/Babu_Frik.jpg/revision/latest/scale-to-width-down/1000?cb=20200110054422');
+INSERT INTO character (full_name, alias, gender, picture_url) VALUES ('Vito Corleone', 'Godfather', 'male', 'https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i');
+
+INSERT INTO franchise (name, description) VALUES ('Star Wars', 'A science fiction universe involving space travel, jedi, evil forces and a rebellion seeking the restoration of freedom.');
+
+INSERT INTO franchise (name, description) VALUES ('Godfather', 'Crime drama trilogy revolving around the Corleone family and the Italian mob in New York in the 1900s.');
